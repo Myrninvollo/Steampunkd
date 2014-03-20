f7e9e78cc95f8ac2b156207c96a3af44652cd7c1
Stephen Ray <SR2610Email@Gmail.com>
2014-03-20 18:18:58 +0000

    Normalised File Headers

--- src/main/java/com/sr2610/steampunked/client/render/PipeItemRenderer.java
+++ src/main/java/com/sr2610/steampunked/client/render/PipeItemRenderer.java
@@ -1,14 +1,12 @@
-/**
- * This class was created by <SR2610>. It's distributed as
- * part of the Steampunk'd Mod. Get the Source Code in github:
- * https://github.com/SR2610/steampunkd
- * 
- * Steampunk'd is Open Source and distributed under a
- * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
- * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
- * 
- * File Created @ [5 Mar 2014, 15:15:00 (GMT)]
- */
+/*******************************************************************************
+ * * This class was created by <SR2610>. It's distributed as
+ *  * part of the Steampunk'd Mod. Get the Source Code in github:
+ *  * https://github.com/SR2610/steampunkd
+ *  * 
+ *  * Steampunk'd is Open Source and distributed under a
+ *  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ *  * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ ******************************************************************************/
 package com.sr2610.steampunked.client.render;
 
 import net.minecraft.block.Block;
