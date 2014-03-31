68c51d00b888e6ae431abc4c3505c7b706e80d8f
Stephen Ray <SR2610Email@Gmail.com>
2014-03-31 20:45:32 +0100

    Cleaned up BlockPipe

--- src/main/java/com/sr2610/steampunked/blocks/BlockPipe.java
+++ src/main/java/com/sr2610/steampunked/blocks/BlockPipe.java
@@ -8,12 +8,11 @@
 package com.sr2610.steampunked.blocks;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
@@ -21,6 +20,7 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import com.sr2610.steampunked.lib.Reference;
 import com.sr2610.steampunked.tileentities.TileEntityPipe;
 import com.sr2610.steampunked.utils.Vector3fMax;
 
@@ -34,6 +34,7 @@ public class BlockPipe extends BlockContainer {
 
 	protected BlockPipe(Material mat) {
 		super(mat);
+		this.setCreativeTab(CreativeTabs.tabRedstone);
 
 	}
 
@@ -52,8 +53,6 @@ public class BlockPipe extends BlockContainer {
 	public boolean renderAsNormalBlock() {
 		return false;
 	}
-
-
 
 	@SideOnly(Side.CLIENT)
 	@Override
@@ -99,63 +98,68 @@ public class BlockPipe extends BlockContainer {
 		TileEntity tileEntity = world.getTileEntity(x, y, z);
 
 		if (tileEntity instanceof TileEntityPipe) {
-			
-				float minX = blockBounds.getXMin();
-				float minY = blockBounds.getYMin();
-				float minZ = blockBounds.getZMin();
-				float maxX = blockBounds.getXMax();
-				float maxY = blockBounds.getYMax();
-				float maxZ = blockBounds.getZMax();
 
-				setBlockBounds(minX, minY, minZ, maxX, maxY, maxZ);
-				super.addCollisionBoxesToList(world, x, y, z, axisalignedbb,
-						arraylist, par7Entity);
-				TileEntity tile1 = world.getTileEntity(x, y, z);
-				if (tile1 instanceof TileEntityPipe) {
-					TileEntityPipe tileG = (TileEntityPipe) tile1;
+			float minX = blockBounds.getXMin();
+			float minY = blockBounds.getYMin();
+			float minZ = blockBounds.getZMin();
+			float maxX = blockBounds.getXMax();
+			float maxY = blockBounds.getYMax();
+			float maxZ = blockBounds.getZMax();
 
-					if (tileG.isPipeConnected(ForgeDirection.EAST)) {
-						setBlockBounds(0.0F, minY, minZ, maxX, maxY, maxZ);
-						super.addCollisionBoxesToList(world, x, y, z,
-								axisalignedbb, arraylist, par7Entity);
-					}
+			setBlockBounds(minX, minY, minZ, maxX, maxY, maxZ);
+			super.addCollisionBoxesToList(world, x, y, z, axisalignedbb,
+					arraylist, par7Entity);
+			TileEntity tile1 = world.getTileEntity(x, y, z);
+			if (tile1 instanceof TileEntityPipe) {
+				TileEntityPipe tileG = (TileEntityPipe) tile1;
 
-					if (tileG.isPipeConnected(ForgeDirection.WEST)) {
-						setBlockBounds(minX, minY, minZ, 1.0F, maxY, maxZ);
-						super.addCollisionBoxesToList(world, x, y, z,
-								axisalignedbb, arraylist, par7Entity);
-					}
-
-					if (tileG.isPipeConnected(ForgeDirection.SOUTH)) {
-						setBlockBounds(minX, minY, minZ, maxX, maxY, 1.0F);
-
-						super.addCollisionBoxesToList(world, x, y, z,
-								axisalignedbb, arraylist, par7Entity);
-					}
-					if (tileG.isPipeConnected(ForgeDirection.NORTH)) {
-						setBlockBounds(minX, minY, 0.0F, maxX, maxY, maxZ);
-						super.addCollisionBoxesToList(world, x, y, z,
-								axisalignedbb, arraylist, par7Entity);
-					}
-					if (tileG.isPipeConnected(ForgeDirection.UP)) {
-						setBlockBounds(minX, minY, minZ, maxX, 1.0F, maxZ);
-						super.addCollisionBoxesToList(world, x, y, z,
-								axisalignedbb, arraylist, par7Entity);
-					}
-					if (tileG.isPipeConnected(ForgeDirection.DOWN)) {
-						setBlockBounds(minX, 0.0F, minZ, maxX, maxY, maxZ);
-						super.addCollisionBoxesToList(world, x, y, z,
-								axisalignedbb, arraylist, par7Entity);
-					}
-					setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+				if (tileG.isPipeConnected(ForgeDirection.EAST)) {
+					setBlockBounds(0.0F, minY, minZ, maxX, maxY, maxZ);
+					super.addCollisionBoxesToList(world, x, y, z,
+							axisalignedbb, arraylist, par7Entity);
 				}
+
+				if (tileG.isPipeConnected(ForgeDirection.WEST)) {
+					setBlockBounds(minX, minY, minZ, 1.0F, maxY, maxZ);
+					super.addCollisionBoxesToList(world, x, y, z,
+							axisalignedbb, arraylist, par7Entity);
+				}
+
+				if (tileG.isPipeConnected(ForgeDirection.SOUTH)) {
+					setBlockBounds(minX, minY, minZ, maxX, maxY, 1.0F);
+
+					super.addCollisionBoxesToList(world, x, y, z,
+							axisalignedbb, arraylist, par7Entity);
+				}
+				if (tileG.isPipeConnected(ForgeDirection.NORTH)) {
+					setBlockBounds(minX, minY, 0.0F, maxX, maxY, maxZ);
+					super.addCollisionBoxesToList(world, x, y, z,
+							axisalignedbb, arraylist, par7Entity);
+				}
+				if (tileG.isPipeConnected(ForgeDirection.UP)) {
+					setBlockBounds(minX, minY, minZ, maxX, 1.0F, maxZ);
+					super.addCollisionBoxesToList(world, x, y, z,
+							axisalignedbb, arraylist, par7Entity);
+				}
+				if (tileG.isPipeConnected(ForgeDirection.DOWN)) {
+					setBlockBounds(minX, 0.0F, minZ, maxX, maxY, maxZ);
+					super.addCollisionBoxesToList(world, x, y, z,
+							axisalignedbb, arraylist, par7Entity);
+				}
+				setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
 			}
 		}
-	
+	}
 
 	@Override
 	public TileEntity createNewTileEntity(World var1, int var2) {
 		return new TileEntityPipe();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister par1IconRegister) {
+		blockIcon = par1IconRegister.registerIcon(Reference.ModID + ":pipe");
 	}
 
 }
