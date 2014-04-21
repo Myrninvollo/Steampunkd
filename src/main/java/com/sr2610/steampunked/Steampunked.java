394af0221995a7f3a54354e3184abc0150c52eab
Stephen Ray <SR2610Email@Gmail.com>
2014-04-21 10:47:07 +0100

    Normalised File Headers

--- src/main/java/com/sr2610/steampunked/Steampunked.java
+++ src/main/java/com/sr2610/steampunked/Steampunked.java
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * * This class was created by <SR2610>. It's distributed as * part of the
- * Steampunk'd Mod. Get the Source Code in github: *
- * https://github.com/SR2610/steampunkd * * Steampunk'd is Open Source and
- * distributed under a * Creative Commons Attribution-NonCommercial-ShareAlike
- * 3.0 License * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * This class was created by <SR2610>. It's distributed as part of the
+ * Steampunk'd Mod. Get the Source Code in Github:
+ * https://github.com/SR2610/Steampunkd
+ * 
+ * Steampunk'd is Open Source and distributed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
  ******************************************************************************/
 package com.sr2610.steampunked;
 
@@ -49,7 +51,7 @@ public class Steampunked {
 	@Instance
 	public static Steampunked instance = new Steampunked();
 
-	private GuiHandler guiHandler = new GuiHandler();
+	private final GuiHandler guiHandler = new GuiHandler();
 
 	public static final String STEAMPUNKED = "STEAMPUNKED";
 
